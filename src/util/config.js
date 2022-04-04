8085c945b147df45b9c0277ce142b0bc6484868a
TF <tf@tfs-imac.lan>
2022-04-04 13:30:06 +0100

    config

--- src/util/config.js
+++ src/util/config.js
@@ -2,17 +2,131 @@ export const alphabet = {
   a: {
     title: "A a",
     img: "https://naldzgraphics.net/wp-content/uploads/2014/03/1-alligator-a.jpg",
+    animal: "aligator",
   },
   b: {
     title: "B b",
     img: "https://naldzgraphics.net/wp-content/uploads/2014/03/2-baboon-b.jpg",
+    animal: "baboon",
   },
   c: {
     title: "C c",
-    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/1-alligator-a.jpg",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/3-chameleon-c.jpg",
+    animal: "chameleon",
   },
   d: {
     title: "D d",
     img: "https://naldzgraphics.net/wp-content/uploads/2014/03/4-dolphin-d.jpg",
+    animal: "dolphin",
+  },
+  e: {
+    title: "E e",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/5-elephant-e.jpg",
+    animal: "elephant",
+  },
+  f: {
+    title: "F f",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/6-fox-f.jpg",
+    animal: "fox",
+  },
+  g: {
+    title: "G g",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/7-giraffe-g.jpg",
+    animal: "giraffe",
+  },
+  h: {
+    title: "H h",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/8-hippopotamus.jpg",
+    animal: "hippopotamus",
+  },
+  i: {
+    title: "I i",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/9-ibis-i.jpg",
+    animal: "ibis",
+  },
+  j: {
+    title: "J j",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/10-jellyfish-j.jpg",
+    animal: "jellyfish",
+  },
+  k: {
+    title: "K k",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/11-kangaroo.jpg",
+    animal: "kangaroo",
+  },
+  l: {
+    title: "L l",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/12-lion-l.jpg",
+    animal: "lion",
+  },
+  m: {
+    title: "M m",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/13-meerkat-m.jpg",
+    animal: "meerkat",
+  },
+  n: {
+    title: "N n",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/14-narwhal-n.jpg",
+    animal: "narwhal",
+  },
+  o: {
+    title: "O o",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/15-owl-o.jpg",
+    animal: "owl",
+  },
+  p: {
+    title: "P p",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/17-penguin-p.jpg",
+    animal: "penguin",
+  },
+  q: {
+    title: "Q q",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/18-quail.jpg",
+    animal: "quail",
+  },
+  r: {
+    title: "R r",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/19-rattlesnake-r.jpg",
+    animal: "rattlesnake",
+  },
+  s: {
+    title: "S s",
+    img: "https://naldzgraphics.net/wp-content/uploads/2014/03/20-swan-s.jpg",
+    animal: "swan",
+  },
+  t: {
+    title: "T t",
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/aa898112083085.56287b37df969.jpg",
+    animal: "tiger",
+  },
+  u: {
+    title: "U u",
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/8b606b12083085.56276eedbdb69.jpg",
+    animal: "unau",
+  },
+  v: {
+    title: "V v",
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/1ae24a12083085.56276ecf2d3a6.jpg",
+    animal: "vampire bat",
+  },
+  w: {
+    title: "W w",
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/a82b1b12083085.56276d1d9dc66.jpg",
+    animal: "walrus",
+  },
+  x: {
+    title: "X x",
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/eff58f12083085.56287d6fa0022.jpg",
+    animal: "x-ray tetra",
+  },
+  y: {
+    title: "Y y",
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/2bcd9912083085.56287cb4822fe.jpg",
+    animal: "yak",
+  },
+  z: {
+    title: "Z z",
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/95e8f012083085.56287bf26f728.jpg",
+    animal: "zebra",
   },
 };
