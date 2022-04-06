e1496e13f36669cdcf402e1d02ffefaae5f0234a
Frederick Horne <frederickhorne@fredericks-macbook-pro.home>
2022-04-06 17:53:25 +0200

    minor styling and read me file

--- src/App.js
+++ src/App.js
@@ -5,28 +5,27 @@ import "./normalize.css";
 import "./app.scss";
 
 const App = () => {
-  const [letter, setLetter] = useState("");
+	const [letter, setLetter] = useState("");
 
-  const onKeyDown = (e) => {
-    console.log(e.key);
-    const letterNumber = /^[a-zA-Z]+$/;
-    if (e.key.match(letterNumber) && e.key.length === 1) setLetter(e.key);
-    else {
-      setLetter("");
-    }
-  };
+	const onKeyDown = (e) => {
+		const letterNumber = /^[a-zA-Z]+$/;
+		if (e.key.match(letterNumber) && e.key.length === 1) setLetter(e.key);
+		else {
+			setLetter("");
+		}
+	};
 
-  return (
-    <div
-      className="container"
-      onKeyDown={onKeyDown}
-      tabIndex="0"
-      style={{ height: "100vh" }}
-    >
-      <Welcome onKeyDown={onKeyDown} letter={letter} />
-      {letter && <Letter letter={letter} />}
-    </div>
-  );
+	return (
+		<div
+			className="container"
+			onKeyDown={onKeyDown}
+			tabIndex="0"
+			style={{ height: "100vh" }}
+		>
+			<Welcome onKeyDown={onKeyDown} letter={letter} />
+			{letter && <Letter letter={letter} />}
+		</div>
+	);
 };
 
 export default App;
